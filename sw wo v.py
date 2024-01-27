ade079e3cdc216314b843eed7831b9bf7f1fbbd3
chandrasekhar1207 <157876120+chandrasekhar1207@users.noreply.github.com>
2024-01-27 13:15:18 +0530

    Add files via upload

---
+++ sw wo v.py
@@ -0,0 +1,7 @@
+a=23
+b=34
+print(a)
+print(b)
+a,b=b,a
+print(a)
+print(b)
---
+++ sw wu v.py
@@ -0,0 +1,11 @@
+a=24
+b=23
+print("before swapping")
+print(a)
+print(b)
+print("after swapping with using third variable")
+temp=a
+a=b
+b=temp
+print(a)
+print(b)
